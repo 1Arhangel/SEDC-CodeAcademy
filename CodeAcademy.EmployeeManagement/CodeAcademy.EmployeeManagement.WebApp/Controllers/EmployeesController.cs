af9848cfa2b3a3b00aad60e826160e27f5a9f50d
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-05-06 19:49:25 +0200

    ASP.NET MVC
    EmployeeManagement

---
+++ CodeAcademy.EmployeeManagement/CodeAcademy.EmployeeManagement.WebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeAcademy.EmployeeManagement.WebApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Create(string name, string surname, string jobposition)
+        {
+            ViewBag.Name = name;
+            ViewBag.Surname = surname;
+            ViewBag.JobPosition = jobposition;
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        
+        //employees/delete/(int id)
+        [HttpPost]
+        public bool Delete(int id)
+        {
+            return false;
+        }
+    }
+}
---
+++ CodeAcademy.EmployeeManagement/CodeAcademy.EmployeeManagement.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeAcademy.EmployeeManagement.WebApp.MvcApplication" Language="C#" %>
