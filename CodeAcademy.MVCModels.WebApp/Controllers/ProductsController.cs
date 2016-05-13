96cb753ca0924da372425706d2a031be3a5eae06
Stojkovski Vasko <stojkovski.vasko.vs@gmail.com>
2016-05-13 21:11:49 +0200

    M OF MVC
    Models

---
+++ CodeAcademy.MVCModels.WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using CodeAcademy.MVCModels.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeAcademy.MVCModels.WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        Database db = new Database();
+        // GET: Products
+        public ActionResult Index(string query)
+        {
+
+
+            
+
+            List<Product> products = db.GetProducts();
+
+            if (!string.IsNullOrEmpty(query))
+            {
+                query = query.ToLower();
+                products = products.Where(x => x.Name.ToLower().Contains(query)).ToList();
+
+            
+            }
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.AddProduct(product);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Product product = db.GetProducts().FirstOrDefault(x => x.ID == id);
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Product product)
+        {
+            db.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Search()
+        {
+
+            List<Product> products = db.GetProducts();
+
+            return View(products);
+        }
+        //
+    }
+}
---
+++ CodeAcademy.MVCModels.WebApp/Models/Database.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CodeAcademy.MVCModels.WebApp.Models
+{
+    public class Database
+    {
+        
+
+        public Database()
+        {
+            if(HttpContext.Current.Application["Products"] == null)
+            HttpContext.Current.Application["Products"]= new List<Product>();
+        }
+
+        public List<Product> GetProducts()
+        {
+            return HttpContext.Current.Application["Products"] as List<Product>;
+        }
+
+        public void AddProduct(Product product)
+        {
+            //get current list of products we have in memory
+            List<Product> dbProducts = HttpContext.Current.Application["Products"] as List<Product>;
+            //add the new product
+            dbProducts.Add(product);
+            // return back in memory the new list with the newly added product
+            HttpContext.Current.Application["Products"] = dbProducts;
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            List<Product> dbProducts = HttpContext.Current.Application["Products"] as List<Product>;
+            //remove 
+            Product productToDelete = dbProducts.FirstOrDefault(x => x.ID == product.ID);
+
+            if (productToDelete != null)
+            {
+                dbProducts.Remove(productToDelete);
+            }
+            // return back in memory the new list with the newly added product
+            HttpContext.Current.Application["Products"] = dbProducts;
+        }
+
+
+    }
+}
